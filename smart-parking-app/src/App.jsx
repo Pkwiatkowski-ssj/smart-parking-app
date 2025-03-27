b8f420f417eecf0a43f9204e1d7bd9b0d1aa8a69
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-03-27 09:53:24 -0400

    deleted extra src file

--- smart-parking-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MapScreen from '../../src/MapScreen';
-
-function App() {
-  console.log("App is rendering MapScreen");
-  return <MapScreen />;
-}
-
-export default App;
